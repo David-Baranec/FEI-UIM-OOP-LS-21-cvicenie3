554f478f011bb2860abbb7c6d7188d9f6eef2da5
David_Baranec <Hyb.3.fur.zix>
2021-03-03 08:40:42 +0100

    pokus2

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -27,6 +27,6 @@ public class Main {
 
         System.out.println(auto.getStavNadrze());
         System.out.println(auto.currentState());
-        System.out.println("pokus1");
+        System.out.println("pokus2");
     }
 }
